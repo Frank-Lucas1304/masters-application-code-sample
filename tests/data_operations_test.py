32258a8d6bd95e6c1943058caa32ee82d7922021
Frank-Lucas1304 <frank-lucas.pantazis@mail.mcgill.ca>
2025-12-02 21:13:58 +0100

    refactor: cleaned up tests

--- tests/data_operations_test.py
+++ tests/data_operations_test.py
@@ -3,20 +3,23 @@ from daq import *
 
 
 class TestDataField:
-    # TODO - Add messages after assert
     @pytest.fixture()
     def data_field_obj(self):
         return DataField("raw_value",2,2, ModbusTcpClient.DATATYPE.FLOAT32,"Raw Value [units]")
 
     def test_convert_from_register(self,data_field_obj):
+        # Gets input from pymodbus library method
         value = ModbusTcpClient.convert_to_registers(0.04355,ModbusTcpClient.DATATYPE.FLOAT32)
+        # Testing projects custom implementation
         output = data_field_obj.convert(value, address_offset=2)
-        assert output == 0.044 # rounding is expected
+        assert output == 0.044, "Converts register encoded data to 32 bit float." # rounding is expected
 
     def test_convert_to_register(self,data_field_obj):
-        value = ModbusTcpClient.convert_from_registers([0,1],ModbusTcpClient.DATATYPE.FLOAT32)
+        # Gets input from pymodbus library method
+        value = ModbusTcpClient.convert_from_registers([0,1],ModbusTcpClient.DATATYPE.FLOAT32) # value = 1.401298464324817e-45
+        # Testing projects custom implementation
         output = data_field_obj.revert([value])
-        assert output == [0,1]# rounding is expected
+        assert output == [0,1], "Converts register encoded data to 32 bit float "# rounding is expected
 
 class TestDataGroup:
     @pytest.fixture()
@@ -34,7 +37,7 @@ class TestDataGroup:
     def test_group_field_count(self, sample_schema):
         """Test that field group contains correct number of fields."""
         group = sample_schema.define_group(['serial_number', 'firmware_version'])
-        assert len(group.fields) == 2
+        assert len(group.fields) == 2, "Data field group should only have 2 fields"
 
     def test_group_header_generation(self, sample_schema,timezone_info):
         """Test that headers are correctly extracted from fields."""
@@ -43,14 +46,5 @@ class TestDataGroup:
         # First header should be timestamps
         assert 'timestamp' in group.headers[0].lower()
         # Subsequent headers from field definitions
-        assert len(group.headers) == 3  # timestamp + 2 fields
-
-    def test_convert_all_returns_correct_count(self, sample_schema,timezone_info):
-        """Test that convert_all returns one value per field."""
-        group = sample_schema.define_group(['serial_number', 'firmware_version'],timezone_info)
-
-        # Mock register data
-        mock_registers = [0] * 20
-        converted = group.convert_all(mock_registers)
-
-        assert len(converted) == 2  # One per field
+        assert len(group.headers) == 3, "There must be 3 headers "
+        assert group.headers[1] == "Serial Number" and group.headers[2] == "Firmware Version", "The Seriral Number header should appear before the Firmware Version header."
